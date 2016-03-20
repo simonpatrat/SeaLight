732a07463152d39d200635d8448874e5a31330b5
simonpatrat <simonpatrat@users.noreply.github.com>
2016-03-20 18:23:11 +0100

    Big Update

--- js/site.js
+++ js/site.js
@@ -96,7 +96,7 @@
 
                 
         // Fancybox initialisation
-        $(".portfolio-item-icons").fancybox();
+        $(".portfolio-item-icons a").fancybox();
 
         
         // Mobile Navigation behaviour
@@ -115,6 +115,43 @@
 
         });
         
+        // Modals
+        var $modalOverlay = $('<div class="modal-overlay"></div>'); 
+        $('[data-toggle="modal"]').on('click', function(event) {
+          event.preventDefault();
+          $('.modal-overlay').detach().remove();
+          var $this = $(this);
+          var $modal = $($this.data('modal-id')); 
+
+          $('body').prepend($modalOverlay);
+          
+          $('.modal-overlay').addClass('overlay-visible').fadeIn(50, function() {
+              $('body').addClass('ovf-hidden');
+              $modal.fadeIn(300, function() {
+                $(this).addClass('modal-visible');
+                $(this).animate({
+                  scrollTop: 0
+                },0);
+              });
+          });
+            
+        });
+
+
+
+        $(document).on('click', '.modal-overlay, .modal-close', function(event) {
+          event.stopPropagation();
+          console.log('clicked !');
+          $('.modal').delay(200).fadeOut(300, function() {
+            $(this).removeClass('modal-visible');
+            $('.modal-overlay').delay(300).fadeOut(300, function() {
+              $(this).removeClass('overlay-visible');
+              
+              $('body').removeClass('ovf-hidden');
+
+            });
+          });  
+        });
  
     });
 
